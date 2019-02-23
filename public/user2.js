942f65d90961653eff094c59481c6ae974c1cea8
Michael Kwan <michaelburps@gmail.com>
2019-02-23 00:52:38 -0600

    12:52 AM

---
+++ public/user2.js
@@ -0,0 +1,14 @@
+function updateName(newname) {
+  document.getElementById('namebar').innerHTML = "Hello " + newname;
+}
+
+function sethealth(percent) {
+  $('#healthbar1').width(percent+"%");
+  $('#healthbar1').html(percent +" \\ 100 HP");
+}
+
+function settimer(duration) {
+  $('#timer').css('width', function(i){
+    return $(this).width() + (i * 50);
+});
+}

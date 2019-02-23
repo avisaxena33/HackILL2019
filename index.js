b067ebd26b98e7bcf46c7fc86a0bdce58655d5ad
Aviral Saxena <avirals2@illinois.edu>
2019-02-22 21:48:43 -0600

    swag1231232312

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-var express = require('express')();
+var express = require('express');
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
 //App Setup
 var app = express();
+
+app.get('/', function(req, res){
+  res.send('<h1>Hello world</h1>');
+});
+
+app.listen(3000, function(){
+  console.log('listening on *:3000');
+});
